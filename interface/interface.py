767df792cf7d3a98c9a63e3f498ee88e3210bb52
Aude Vendeuvre <aude.vendeuvre@etu.minesparis.psl.eu>
2025-06-30 10:45:04 +0200

    interface

--- interface/interface.py
+++ interface/interface.py
@@ -1,5 +1,6 @@
 import streamlit as st
 
+conn = st.connection("database",type="sql") #il faut mettre le bon nom je pense, et il faut créer un fichier secrets.toml
 data = ... #les données à récupérer
 
 st.markdown("Optimization of aluminium alloys")
@@ -14,8 +15,8 @@ ID_SCRAP = data[external_scrap]['scrap_name']
 
 
 if st.checkbox ('Show data'):
-    data_visualization = ... #les données à visualiser dans un tableau
+    data_visualization = conn.query("SELECT * FROM ...") #les données à visualiser dans un tableau, il faut le nom de la table sql
     data_visualization
 
 if st.button('Optimize'):
-    scrap_column, no_scrap_column = st.column(2)
+    scrap_column, no_scrap_column = st.columns(2)
