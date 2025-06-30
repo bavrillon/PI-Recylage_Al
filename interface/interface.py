546f3da0cdcd6355b98baf235a1de1c8d9353b77
Aude Vendeuvre <aude.vendeuvre@etu.minesparis.psl.eu>
2025-06-30 11:54:00 +0200

    interface

--- interface/interface.py
+++ interface/interface.py
@@ -18,11 +18,31 @@ with c2:
     ID_SCRAP = data[external_scrap]['scrap_name']
 
 side=st.sidebar
-side.text_input("What scrap do you want to add?")
-if side.button("Add"):
+side.text_input("What scrap do you want to add?") #je pense pas que text_input soit le bon truc, ou alors il faut se mettre d'accord avec grégoire
+                                                  # pour par ex séparer les colonnes par des virgules
+if side.button("Add scrap"):
     conn.query("INSERT INTO data VALUES ... ") #rajouter la chute à la database
 
-#faire pareil pour tous les paramètres
+side.text_input("What alloy do you want to add?")
+if side.button("Add alloy"):
+    conn.query("INSERT INTO data VALUES ... ") #rajouter à la database
+
+side.text_input("What site do you want to add?")
+if side.button("Add site"):
+    conn.query("INSERT INTO data VALUES ... ") #rajouter à la database
+
+side.text_input("What raw material do you want to add?")
+if side.button("Add material"):
+    conn.query("INSERT INTO data VALUES ... ") #rajouter à la database
+
+side.text_input("What currency do you want to add?")
+if side.button("Add currency"):
+    conn.query("INSERT INTO data VALUES ... ") #rajouter à la database
+
+side.text_input("What recycling cost do you want to add?")
+if side.button("Add recycling cost"):
+    conn.query("INSERT INTO data VALUES ... ") #rajouter à la database
+
 
 if st.checkbox ('Show data'):
     data_visualization = conn.query("SELECT * FROM alloy, raw_material, recycling_costs, currency") #les données à visualiser dans un tableau
