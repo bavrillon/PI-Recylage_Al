016520d3c9eedce717e0352e981aa849ab10d175
Aude Vendeuvre <aude.vendeuvre@etu.minesparis.psl.eu>
2025-06-30 09:44:35 +0200

    interface

--- interface.py
+++ interface.py
@@ -2,6 +2,8 @@ import streamlit as st
 
 data = ... #les données à récupérer
 
+st.markdown("Optimisation des alliages d'aluminium")
+
 site = st.selectbox('Which factory?', data['site'])
 'You selected:', site
 ID_SITE = data[site]['site_code']
@@ -12,5 +14,5 @@ ID_SCRAP = data[external_scrap]['scrap_name']
 
 
 if st.checkbox ('Show data'):
-    data_visualization = ... #les données à visualiser
+    data_visualization = ... #les données à visualiser dans un tableau
     data_visualization
