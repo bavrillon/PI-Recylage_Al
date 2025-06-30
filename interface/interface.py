94e9e8edf20c2b2f35c824e7de637d85b22eb01b
Aude Vendeuvre <aude.vendeuvre@etu.minesparis.psl.eu>
2025-06-30 11:10:31 +0200

    interface

--- interface/interface.py
+++ interface/interface.py
@@ -5,13 +5,17 @@ data = ... #les données à récupérer
 
 st.markdown("Optimization of aluminium alloys")
 
-site = st.selectbox('Which factory?', data['site']['name'])
-'You selected:', site
-ID_SITE = data[site]['site_code']
+c1, c2 = st.columns(2)
 
-external_scrap = st.selectbox('Which scrap?', data['external_scrap']['scrap_name'])
-'You selected:', external_scrap
-ID_SCRAP = data[external_scrap]['scrap_name']
+with c1:
+    site = st.selectbox('Which factory?', data['site']['name'])
+    'You selected:', site
+    ID_SITE = data[site]['site_code']
+
+with c2:
+    external_scrap = st.selectbox('Which scrap?', data['external_scrap']['scrap_name'])
+    'You selected:', external_scrap
+    ID_SCRAP = data[external_scrap]['scrap_name']
 
 side=st.sidebar
 side.text_input("What scrap do you want to add?")
