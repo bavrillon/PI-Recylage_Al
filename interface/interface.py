a441c912882b48c4bfbba1b877353f9cdf83d1d6
Aude Vendeuvre <aude.vendeuvre@etu.minesparis.psl.eu>
2025-06-30 15:08:49 +0200

    avec data_editor

--- interface/interface.py
+++ interface/interface.py
@@ -17,36 +17,11 @@ with c2:
     'You selected:', external_scrap
     ID_SCRAP = data[external_scrap]['scrap_name']
 
-side=st.sidebar
-side.text_input("What scrap do you want to add?") #je pense pas que text_input soit le bon truc, ou alors il faut se mettre d'accord avec grégoire
-                                                  # pour par ex séparer les colonnes par des virgules
-if side.button("Add scrap"):
-    conn.query("INSERT INTO data VALUES ... ") #rajouter la chute à la database
-
-side.text_input("What alloy do you want to add?")
-if side.button("Add alloy"):
-    conn.query("INSERT INTO data VALUES ... ") #rajouter à la database
-
-side.text_input("What site do you want to add?")
-if side.button("Add site"):
-    conn.query("INSERT INTO data VALUES ... ") #rajouter à la database
-
-side.text_input("What raw material do you want to add?")
-if side.button("Add material"):
-    conn.query("INSERT INTO data VALUES ... ") #rajouter à la database
-
-side.text_input("What currency do you want to add?")
-if side.button("Add currency"):
-    conn.query("INSERT INTO data VALUES ... ") #rajouter à la database
-
-side.text_input("What recycling cost do you want to add?")
-if side.button("Add recycling cost"):
-    conn.query("INSERT INTO data VALUES ... ") #rajouter à la database
-
 
 if st.checkbox ('Show data'):
-    data_visualization = conn.query("SELECT * FROM alloy, raw_material, recycling_costs, currency") #les données à visualiser dans un tableau
-    st.dataframe(data_visualization)
+    edited_data = st.data_editor(data, num_rows="dynamic")
+    edited_data
+    data = edited_data
 
 if st.button('Optimize'):
     scrap_column, no_scrap_column = st.columns(2)
