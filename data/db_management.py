4082961780e6b97e86fa391463f1fd61a4b2a436
Honoré Boiarsky <honore.boiarsky@etu.minesparis.psl.eu>
2025-06-30 15:54:08 +0200

    15h51

--- data/db_management.py
+++ data/db_management.py
@@ -84,7 +84,7 @@ class Database:
         row = cursor.fetchone()
         if row is None:
             conn.close()
-<<<<<<< HEAD
+
             raise ValueError("Raw material not found")
         cost, currency = row
 
@@ -238,9 +238,4 @@ class Database:
 
 
 
-=======
-            raise ValueError("Currency not found")
-        rate = float(rate_row[0])
-    conn.close()
-    return float(cost) / rate
->>>>>>> 653022eb82e3d83fc75893afcfa7ca975cba196e
+=
