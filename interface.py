2cad9edf0d916112c1b346a886ad5ff7ce0afe02
Aude Vendeuvre <aude.vendeuvre@etu.minesparis.psl.eu>
2025-06-30 09:38:59 +0200

    interface

--- interface.py
+++ interface.py
@@ -4,8 +4,11 @@ data = ... #les données à récupérer
 
 site = st.selectbox('Which factory?', data['site'])
 'You selected:', site
+ID_SITE = data[site]['site_code']
+
 external_scrap = st.selectbox('Which scrap?', data['external_scrap'])
 'You selected:', external_scrap
+ID_SCRAP = data[external_scrap]['scrap_name']
 
 
 if st.checkbox ('Show data'):
