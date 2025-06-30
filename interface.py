471c1dddbed08d3cf34aeecc7c25d6acd14f8485
Aude Vendeuvre <aude.vendeuvre@etu.minesparis.psl.eu>
2025-06-30 09:36:46 +0200

    interface

--- interface.py
+++ interface.py
@@ -1,5 +1,13 @@
 import streamlit as st
 
+data = ... #les données à récupérer
+
+site = st.selectbox('Which factory?', data['site'])
+'You selected:', site
+external_scrap = st.selectbox('Which scrap?', data['external_scrap'])
+'You selected:', external_scrap
+
+
 if st.checkbox ('Show data'):
-    data=... #les données à visualiser
-    data
+    data_visualization = ... #les données à visualiser
+    data_visualization
