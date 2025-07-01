f3e119234ec5ed6b1314e7ab87b8e8b72fd073a1
Bastien Avrillon <bastien.avrillon@etu.minesparis.psl.eu>
2025-07-01 15:31:47 +0200

    db fonctionnelle

--- optimisation.py
+++ optimisation.py
@@ -1,4 +1,7 @@
 import pulp
+import data.db_management
+
+db = data.db_management.Database("data.db")
 
 composition_elements = db.get_elements() # ['Si','Fe','Cu',...]
 nb_elements = len(composition_elements) 
@@ -8,7 +11,7 @@ def optimise_co2_with_scrap(id_site, id_alloy, id_scrap):
     co2_raw_materials_and_scrap = co2_raw_materials.append(db.get_co2_scrap(id_scrap))    # List of the co2/t of the raw materials + selected scrap
     raw_materials = db.get_raw_materials()    # List of the ID of the raw materials
     composition_ids = raw_materials.append(id_scrap)   # List of the ID of the raw materials + the scrap to be mixed
-    compostion_alloy_wished = db.get_composition(id_alloy)   
+    compostion_alloy_wished = db.get_composition_alloy(id_alloy)   
 
     # Optimisation pb
     composition = pulp.LpVariable.dicts("calculed composition", composition_ids, cat='Continuous', lowBound=0, upBound=1)
@@ -19,7 +22,7 @@ def optimise_co2_with_scrap(id_site, id_alloy, id_scrap):
     #problem += (pulp.lpSum([composition[i] for i in range(len(composition_ids))]) == 1)
     # Constraint of composition of the alloy :
     for k in range(len(composition_ids)) :  
-        composition_raw_materials_and_scrap = [db.get_composition(id_raw_mat) for id_raw_mat in raw_materials].append(db.get_composition(id_scrap)) # Composition in different elements of the raw materials + the scrap
+        composition_raw_materials_and_scrap = [db.get_composition_raw_material(id_raw_mat) for id_raw_mat in raw_materials].append(db.get_composition_scrap(id_scrap)) # Composition in different elements of the raw materials + the scrap
                                                                                                                                                                         #  Ligne = raw material / scrap.  Colonne = Composition in different elements
         problem += pulp.lpSum([composition[i]*composition_raw_materials_and_scrap[j][k] for (j,i) in enumerate(composition_ids)]) == compostion_alloy_wished[k]
                                 
@@ -33,7 +36,7 @@ def optimise_co2_without_scrap(id_site, id_alloy):
     co2_raw_materials = db.get_co2_raw_materials()    # List of the co2/t of the raw materials 
     raw_materials = db.get_raw_materials()    # List of the ID of the raw materials
     composition_ids = raw_materials        # List of the ID of the raw materials + the scrap to be mixed
-    compostion_alloy_wished = db.get_composition(id_alloy)   
+    compostion_alloy_wished = db.get_compositio_alloy(id_alloy)   
 
     # Optimisation pb
     composition = pulp.LpVariable.dicts("calculed composition", composition_ids, cat='Continuous', lowBound=0, upBound=1)
@@ -44,7 +47,7 @@ def optimise_co2_without_scrap(id_site, id_alloy):
     #problem += (pulp.lpSum([composition[i] for i in range(len(composition_ids))]) == 1)
     # Constraint of composition of the alloy :
     for k in range(len(composition_ids)) :  
-        composition_raw_materials = [db.get_composition(id_raw_mat) for id_raw_mat in raw_materials]  # Composition in different elements of the raw materials 
+        composition_raw_materials = [db.get_composition_raw_material(id_raw_mat) for id_raw_mat in raw_materials]  # Composition in different elements of the raw materials 
                                                                                                                                 #  Ligne = raw material.  Colonne = Composition in different elements.
         problem += pulp.lpSum([composition[i]*composition_raw_materials[j][k] for (j,i) in enumerate(composition_ids)]) == compostion_alloy_wished[k]
     
@@ -60,7 +63,7 @@ def optimise_cost_with_scrap(id_site, id_alloy, id_scrap):
     cost_raw_materials_and_scrap = cost_raw_materials.append(db.get_cost_scrap(id_site, id_scrap))    # List of the cost of the raw materials + selected scrap
     raw_materials = db.get_raw_materials()    # List of the ID of the raw materials
     composition_ids = raw_materials.append(id_scrap)   # List of the ID of the raw materials + the scrap to be mixed
-    compostion_alloy_wished = db.get_composition(id_alloy)   
+    compostion_alloy_wished = db.get_composition_alloy(id_alloy)   
 
     # Optimisation pb
     composition = pulp.LpVariable.dicts("calculed composition", composition_ids, cat='Continuous', lowBound=0, upBound=1)
@@ -71,7 +74,7 @@ def optimise_cost_with_scrap(id_site, id_alloy, id_scrap):
     #problem += (pulp.lpSum([composition[i] for i in range(len(composition_ids))]) == 1)
     # Constraint of composition of the alloy :
     for k in range(len(composition_ids)) :  
-        composition_raw_materials_and_scrap = [db.get_composition(id_raw_mat) for id_raw_mat in raw_materials].append(db.get_composition(id_scrap)) # Composition in different elements of the raw materials + the scrap
+        composition_raw_materials_and_scrap = [db.get_composition_raw_material(id_raw_mat) for id_raw_mat in raw_materials].append(db.get_composition_scrap(id_scrap)) # Composition in different elements of the raw materials + the scrap
                                                                                                                                                                         #  Ligne = raw material / scrap.  Colonne = Composition in different elements
         problem += pulp.lpSum([composition[i]*composition_raw_materials_and_scrap[j][k] for (j,i) in enumerate(composition_ids)]) == compostion_alloy_wished[k]
     
@@ -86,7 +89,7 @@ def optimise_cost_without_scrap(id_site, id_alloy):
     cost_raw_materials = db.get_cost_raw_materials(id_site)    # List of the cost of the raw materials 
     raw_materials = db.get_raw_materials()    # List of the ID of the raw materials
     composition_ids = raw_materials        # List of the ID of the raw materials + the scrap to be mixed
-    compostion_alloy_wished = db.get_composition(id_alloy)   
+    compostion_alloy_wished = db.get_composition_alloy(id_alloy)   
 
     # Optimisation pb
     composition = pulp.LpVariable.dicts("calculed composition", composition_ids, cat='Continuous', lowBound=0, upBound=1)
@@ -97,7 +100,7 @@ def optimise_cost_without_scrap(id_site, id_alloy):
     #problem += (pulp.lpSum([composition[i] for i in range(len(composition_ids))]) == 1)
     # Constraint of composition of the alloy :
     for k in range(len(composition_ids)) :  
-        composition_raw_materials = [db.get_composition(id_raw_mat) for id_raw_mat in raw_materials]  # Composition in different elements of the raw materials 
+        composition_raw_materials = [db.get_composition_raw_material(id_raw_mat) for id_raw_mat in raw_materials]  # Composition in different elements of the raw materials 
                                                                                                                                 #  Ligne = raw material.  Colonne = Composition in different elements.
         problem += pulp.lpSum([composition[i]*composition_raw_materials[j][k] for (j,i) in enumerate(composition_ids)]) == compostion_alloy_wished[k]
     
@@ -111,7 +114,7 @@ def optimise_cost_without_scrap(id_site, id_alloy):
 def optimise_utilisation_scrap(id_site, id_alloy, id_scrap):
     raw_materials = db.get_raw_materials()    # List of the ID of the raw materials
     composition_ids = raw_materials.append(id_scrap)   # List of the ID of the raw materials + the scrap to be mixed
-    compostion_alloy_wished = db.get_composition(id_alloy)   
+    compostion_alloy_wished = db.get_composition_alloy(id_alloy)   
 
     # Optimisation pb
     composition = pulp.LpVariable.dicts("calculed composition", composition_ids, cat='Continuous', lowBound=0, upBound=1)
@@ -122,7 +125,7 @@ def optimise_utilisation_scrap(id_site, id_alloy, id_scrap):
     #problem += (pulp.lpSum([composition[i] for i in range(len(composition_ids))]) == 1)
     # Constraint of composition of the alloy :
     for k in range(len(composition_ids)) :  
-        composition_raw_materials_and_scrap = [db.get_composition(id_raw_mat) for id_raw_mat in raw_materials].append(db.get_composition(id_scrap)) # Composition in different elements of the raw materials + the scrap
+        composition_raw_materials_and_scrap = [db.get_composition_raw_material(id_raw_mat) for id_raw_mat in raw_materials].append(db.get_composition_scrap(id_scrap)) # Composition in different elements of the raw materials + the scrap
                                                                                                                                                                         #  Ligne = raw material / scrap.  Colonne = Composition in different elements
         problem += pulp.lpSum([composition[i]*composition_raw_materials_and_scrap[j][k] for (j,i) in enumerate(composition_ids)]) == compostion_alloy_wished[k]
     
