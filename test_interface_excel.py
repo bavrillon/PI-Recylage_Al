9e9c902872d1de9c5948a95642d2a1cd7c2c56fd
Elyes Beloucif <elyes.beloucif@etu.minesparis.psl.eu>
2025-07-03 16:54:17 +0200

    test excel sans méthodes pour get co2 & prix

--- test_interface_excel.py
+++ test_interface_excel.py
@@ -26,6 +26,7 @@ if uploaded_file is not None:
         st.error("The uploaded file does not have the correct format. Please check the example file.")
     
     df_display = pd.DataFrame(columns=['Alloy', 'Optimization', 'Use scrap', 'Product', 'Price', 'CO2', 'Mass', 'Si', 'Fe', 'Cu', 'Mn', 'Mg', 'Cr', 'Zn', 'Ti'])
+    scrap_tables = {}
 
     for line in df_scrap.itertuples(index=False):
         scrap_name, si, fe, cu, mn, mg, cr, zn, ti, shape, scrap_purchasing_cost_per_t, transportation_cost_per_t, currency = line
@@ -64,4 +65,68 @@ if uploaded_file is not None:
             alloy_site_code = row['site_code']
             alloy_composition_id = row['composition_id']
             alloy_id = row['alloy_id']
-            
+
+            # if the program returns a ValueError, fill the excel table with NaN
+            try:
+                optimized_co2_with_scrap = [x*100 for x in db.optimise_co2_with_scrap(id_site=alloy_site_code, id_alloy=alloy_id, id_scrap=0)] #composition in %
+            except ValueError:
+                df_display = df_display.append({'Alloy': alloy_name, 'Optimization': 'CO2-Error', 'Use scrap': 'Yes', 'Product': 'NaN', 'Price': 'NaN', 'CO2': 'NaN', 'Mass': 'NaN',
+                                                'Si': 'NaN', 'Fe': 'NaN', 'Cu': 'NaN', 'Mn': 'NaN', 'Mg': 'NaN', 'Cr': 'NaN', 'Zn': 'NaN', 'Ti': 'NaN'}, ignore_index=True)
+                continue
+
+            #adding the data to the dataframe with the methods to get price and CO2
+            #then, add last line with total values of all the components
+
+            try:
+                optimized_co2_without_scrap = [x*100 for x in db.optimise_co2_without_scrap(id_site=alloy_site_code, id_alloy=alloy_id)]
+            except ValueError:
+                df_display = df_display.append({'Alloy': alloy_name, 'Optimization': 'CO2-Error', 'Use scrap': 'No', 'Product': 'NaN', 'Price': 'NaN', 'CO2': 'NaN', 'Mass': 'NaN',
+                                                'Si': 'NaN', 'Fe': 'NaN', 'Cu': 'NaN', 'Mn': 'NaN', 'Mg': 'NaN', 'Cr': 'NaN', 'Zn': 'NaN', 'Ti': 'NaN'}, ignore_index=True)
+                continue
+
+            #adding the data to the dataframe with the methods to get price and CO2
+            #then, add last line with total values of all the components
+
+            try:
+                optimized_price_with_scrap = db.optimise_price_with_scrap(id_site=alloy_site_code, id_alloy=alloy_id, id_scrap=0)
+            except ValueError:
+                df_display = df_display.append({'Alloy': alloy_name, 'Optimization': 'Price-Error', 'Use scrap': 'Yes', 'Product': 'NaN', 'Price': 'NaN', 'CO2': 'NaN', 'Mass': 'NaN',
+                                                'Si': 'NaN', 'Fe': 'NaN', 'Cu': 'NaN', 'Mn': 'NaN', 'Mg': 'NaN', 'Cr': 'NaN', 'Zn': 'NaN', 'Ti': 'NaN'}, ignore_index=True)
+                continue
+
+            #adding the data to the dataframe with the methods to get price and CO2
+            #then, add last line with total values of all the components
+
+            try:
+                optimized_price_without_scrap = db.optimise_price_without_scrap(id_site=alloy_site_code, id_alloy=alloy_id)
+            except ValueError:
+                df_display = df_display.append({'Alloy': alloy_name, 'Optimization': 'Price-Error', 'Use scrap': 'No', 'Product': 'NaN', 'Price': 'NaN', 'CO2': 'NaN', 'Mass': 'NaN',
+                                                'Si': 'NaN', 'Fe': 'NaN', 'Cu': 'NaN', 'Mn': 'NaN', 'Mg': 'NaN', 'Cr': 'NaN', 'Zn': 'NaN', 'Ti': 'NaN'}, ignore_index=True)
+                continue
+
+            #adding the data to the dataframe with the methods to get price and CO2
+            #then, add last line with total values of all the components
+
+            try:
+                optimized_mass_with_scrap = db.optimise_utilisation_scrap(id_site=alloy_site_code, id_alloy=alloy_id, id_scrap=0)
+            except ValueError:
+                df_display = df_display.append({'Alloy': alloy_name, 'Optimization': 'Mass-Error', 'Use scrap': 'Yes', 'Product': 'NaN', 'Price': 'NaN', 'CO2': 'NaN', 'Mass': 'NaN',
+                                                'Si': 'NaN', 'Fe': 'NaN', 'Cu': 'NaN', 'Mn': 'NaN', 'Mg': 'NaN', 'Cr': 'NaN', 'Zn': 'NaN', 'Ti': 'NaN'}, ignore_index=True)
+                continue
+
+            #adding the data to the dataframe with the methods to get price and CO2
+            #then, add last line with total values of all the components
+
+        #Now, we can add the dataframe to the dict of tables to be displayed in the Streamlit app and put in sheets for the Excel file, each sheet for a different scrap
+        scrap_tables[scrap_name] = df_display
+
+    # Display the DataFrame in Streamlit
+    st.write(df_display)
+
+#Save the list of DataFrames to an Excel file
+if st.button("Save to Excel"):
+    with pd.ExcelWriter("optimized_scrap_data.xlsx") as writer:
+        for scrap_name, df in scrap_tables.items():
+            df.to_excel(writer, sheet_name=scrap_name, index=False)
+    st.success("Data saved to optimized_scrap_data.xlsx")
+    st.download_button(label="Download Excel file", data="optimized_scrap_data.xlsx", file_name="optimized_scrap_data.xlsx")
