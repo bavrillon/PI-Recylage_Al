b8ff5c7546c265f9c6a73f5c6612ce819c83b83a
Aude Vendeuvre <aude.vendeuvre@etu.minesparis.psl.eu>
2025-07-04 15:32:06 +0200

    pages

---
+++ main.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+main_page = st.Page("interface.py", title="Optimized use of a scrap")
+page_2 = st.Page("test_interface_excel.py", title="Import multiple scraps")
+
+pg = st.navigation([main_page, page_2])
+
+pg.run()
