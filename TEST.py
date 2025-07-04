4fdd2ade544d691d320e887da76c1e528dc8002e
bavrillon <bastien.avrillon@etu.minesparis.psl.eu>
2025-07-04 19:34:18 +0200

    Delete TEST.py

--- TEST.py
+++
@@ -1,7 +0,0 @@
-from data.db_tools import Database
-from os import path
-
-db = Database(path.join(path.dirname(path.realpath(__file__)), "data", "data.db"))
-
-test = db.get_cost_scrap('PAR', 'S0')
-print(test)
