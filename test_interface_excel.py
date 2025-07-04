3be53a50693d5353dfb7e66d8db037c0b98b07fd
Elyes Beloucif <elyes.beloucif@etu.minesparis.psl.eu>
2025-07-04 15:05:58 +0200

    bazar

--- test_interface_excel.py
+++ test_interface_excel.py
@@ -2,12 +2,15 @@ from os import path
 import streamlit as st
 from data.db_tools import Database
 import pandas as pd
+import io
+from sqlalchemy import text
 
 
 # Import the database
 db = Database(path.join(path.dirname(__file__), "data.db"))
-conn = st.connection("data_db",type="sql", connect_args={"timeout": 5})
+conn = st.connection("data_db", type="sql", connect_args={"timeout": 5})
 
+st.title("Scrap data upload")
 
 sites = conn.query("SELECT * FROM site") #returns a DataFrame
 alloys = conn.query("SELECT * FROM alloy")
@@ -22,12 +25,14 @@ shape_types = conn.query("SELECT * FROM shape_type")
 
 
 # Testing excel file upload
-st.write("You can upload here an Excel file with the scrap data. Please be careful to use the correct format, like the example provided.")
+st.write("You can upload here an Excel file with the scrap data. Please be careful to use the correct format (and a xlsx file), like the example provided.")
 st.download_button(label="Download example file", data="interface/data_example_interface.py", file_name="data_example_interface.xlsx")
 uploaded_file = st.file_uploader("Upload Excel file", type=["xlsx"])
 
 if uploaded_file is not None:
-    df_scrap = pd.read_excel(uploaded_file)
+    file_bytes = uploaded_file.read()
+    file_buffer = io.BytesIO(file_bytes)
+    df_scrap = pd.read_excel(file_buffer, engine='openpyxl')
     columns = df_scrap.columns.tolist()
     if columns != ["scrap_name", "Si", "Fe", "Cu", "Mn", "Mg", "Cr", "Zn", "Ti",
                    "shape", "scrap_purchasing_cost_per_t", "transportation_cost_per_t", "currency"]:
@@ -40,32 +45,31 @@ if uploaded_file is not None:
     for line in df_scrap.itertuples(index=False):
         scrap_name, si, fe, cu, mn, mg, cr, zn, ti, shape, scrap_purchasing_cost_per_t, transportation_cost_per_t, currency = line
         shape_id = conn.query(f"SELECT shape_type_id FROM shape_type WHERE name='{shape}'").iloc[0, 0]
-        currency_id = conn.query(f"SELECT currency_id FROM currency WHERE name='{currency}'").iloc[0, 0]
+        currency_id = conn.query(f"SELECT name FROM currency WHERE name='{currency}'").iloc[0, 0]
         compo_id = conn.query("SELECT COUNT(*) FROM composition").iloc[0, 0] + 1  # the scrap composition ID is the last ID in the composition table
 
-        insert_compo = conn.query("INSERT INTO composition (composition_id, Si, Fe, Cu, Mn, Mg, Cr, Zn, Ti) "
-                                "VALUES (:compo_id, :si, :fe, :cu, :mn, :mg, :cr, :zn, :ti)",
-                                dict(compo_id=compo_id, si=si, fe=fe, cu=cu, mn=mn, mg=mg, cr=cr, zn=zn, ti=ti))
+        insert_compo = text("INSERT INTO composition (composition_id, Si, Fe, Cu, Mn, Mg, Cr, Zn, Ti) VALUES (:compo_id, :si, :fe, :cu, :mn, :mg, :cr, :zn, :ti)")
         with conn.session as session:
-            session.execute(insert_compo)
+            session.execute(insert_compo, 
+                                  dict(compo_id=compo_id, si=si, fe=fe, cu=cu, mn=mn, mg=mg, cr=cr, zn=zn, ti=ti))
             session.commit()
         
-        delete_scrap = db.query("DELETE FROM scrap")
+        delete_scrap = text("DELETE FROM scrap")
         with conn.session as session:
             session.execute(delete_scrap)
             session.commit()
         
-        insert_scrap = db.query("INSERT INTO scrap (scrap_id, scrap_name, composition_id, shape_type_id, scrap_purchasing_cost_per_t, transportation_cost_per_t, currency_id) "
-                                "VALUES (:ID_SCRAP, :scrap_name, :compo_id, :shape_id, :scrap_purchasing_cost_per_t, :transportation_cost_per_t, :currency_id)",
-                                dict(ID_SCRAP=0,
+        insert_scrap = text("""INSERT INTO scrap (scrap_id, scrap_name, composition_id, shape_type_id, scrap_purchasing_cost_per_t, transportation_cost_per_t, currency)
+                                VALUES (:ID_SCRAP, :scrap_name, :compo_id, :shape_id, :scrap_purchasing_cost_per_t, :transportation_cost_per_t, :currency)""")
+        with conn.session as session:
+            session.execute(insert_scrap,
+                            dict(ID_SCRAP=0,
                                      scrap_name=scrap_name,
                                      compo_id=compo_id,
                                      shape_id=shape_id,
                                      scrap_purchasing_cost_per_t=scrap_purchasing_cost_per_t,
                                      transportation_cost_per_t=transportation_cost_per_t,
-                                     currency_id=currency_id))
-        with conn.session as session:
-            session.execute(insert_scrap)
+                                     currency=currency))
             session.commit()
         
         # now, we do the calculations and put it in a new dataframe
@@ -85,10 +89,10 @@ if uploaded_file is not None:
                 optimized_co2_with_scrap = [x*100 for x in db.optimise_co2_with_scrap(id_site=alloy_site_code,
                                                                                       id_alloy=alloy_id, id_scrap=0)] #composition in %
             except ValueError:
-                df_display.append({
-                    'Alloy': alloy_name, 'Optimization': 'CO2-Error', 'Use scrap': 'Yes', 'Product': 'NaN', 'Price': 'NaN',
-                    'CO2': 'NaN', 'Mass': 'NaN', 'Si': 'NaN', 'Fe': 'NaN', 'Cu': 'NaN', 'Mn': 'NaN', 'Mg': 'NaN', 'Cr': 'NaN',
-                    'Zn': 'NaN', 'Ti': 'NaN'}, ignore_index=True)
+                new_line = {'Alloy': alloy_name, 'Optimization': 'CO2-Error', 'Use scrap': 'Yes', 'Product': 'NaN', 'Price': 'NaN',
+                            'CO2': 'NaN', 'Mass': 'NaN', 'Si': 'NaN', 'Fe': 'NaN', 'Cu': 'NaN', 'Mn': 'NaN', 'Mg': 'NaN', 'Cr': 'NaN',
+                            'Zn': 'NaN', 'Ti': 'NaN'}
+                df_display = pd.concat(df_display, pd.DataFrame([new_line], ignore_index=True))
                 continue
 
 
@@ -99,7 +103,7 @@ if uploaded_file is not None:
 
             # first, we add the scrap data
             # to get the composition of each element, we multiply the composition of each of the raw materials composing the element by the mass of the element in the optimized composition
-            df_display.append({
+            new_line = {
                 'Alloy': alloy_name, 'Optimization': 'CO2', 'Use scrap': 'Yes', 'Product': scrap_name,
                 'Price': db.get_cost_scrap(alloy_site_code, id_scrap=0)*optimized_co2_with_scrap[-1],
                 'CO2': 0., 'Mass': optimized_co2_with_scrap[-1],
@@ -110,7 +114,9 @@ if uploaded_file is not None:
                 'Mg': db.get_composition_raw_material(list_id_raw_materials[4])[4]*optimized_co2_with_scrap[-1],
                 'Cr': db.get_composition_raw_material(list_id_raw_materials[5])[5]*optimized_co2_with_scrap[-1],
                 'Zn': db.get_composition_raw_material(list_id_raw_materials[6])[6]*optimized_co2_with_scrap[-1],
-                'Ti': db.get_composition_raw_material(list_id_raw_materials[7])[7]*optimized_co2_with_scrap[-1]})
+                'Ti': db.get_composition_raw_material(list_id_raw_materials[7])[7]*optimized_co2_with_scrap[-1]}
+            df_display = pd.concat(df_display, pd.DataFrame([new_line]), ignore_index=True)
+
             price_total += db.get_cost_scrap(alloy_site_code, id_scrap=0)*optimized_co2_with_scrap[-1]
             mass_total += optimized_co2_with_scrap[-1]
             si_total += db.get_composition_raw_material(list_id_raw_materials[0])[0]*optimized_co2_with_scrap[-1]
