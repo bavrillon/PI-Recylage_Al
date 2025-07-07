ed4fc45f805465f71086d4155273c490383090a4
Elyes Beloucif <elyes.beloucif@etu.minesparis.psl.eu>
2025-07-07 08:54:59 +0200

    changement nom interface_excel dans le main

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 main_page = st.Page("interface.py", title="Optimized use of a scrap")
-page_2 = st.Page("test_interface_excel.py", title="Import multiple scraps")
+page_2 = st.Page("interface_excel.py", title="Import multiple scraps")
 
 pg = st.navigation([main_page, page_2])
 
